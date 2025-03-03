7f74797671b2be59369cc5b7459e31ede2d4c692
umroot <umroot@veteran.encs.concordia.ca>
2025-03-03 16:51:10 -0500

    added the expertiseIntroduction section

--- src/components/Expertise/ExpertiseIntroduction.jsx
+++ src/components/Expertise/ExpertiseIntroduction.jsx
@@ -8,8 +8,50 @@ export default function ExpertiseIntroduction() {
           className="KneeReplacementRealLife"
           alt=""
         />
-        <div className="col s4 offset-s6 description">
-          <span>this is description</span>
+        <div className="col s10 m6 offset-s6 offset-m6 white-text description">
+          <h5>
+            <b>
+              In this page you will read the following about knee replacement :
+            </b>
+          </h5>
+          <ul>
+            <li>
+              <a href="">
+                <i className="material-icons">link</i> &nbsp; What is "knee"
+                replacement
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <i className="material-icons">link</i> &nbsp; Different types of
+                "knee" replacement
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <i className="material-icons">link</i> &nbsp; Who is a candidate
+                for a "knee" replacement?
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <i className="material-icons">link</i> &nbsp; How can I prepare
+                for surgery?
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <i className="material-icons">link</i> &nbsp; What can I expect
+                during recovery?
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <i className="material-icons">link</i> &nbsp; What are the
+                risks?
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
